151dad70e9c1265d847c55bc81946117913ccc5f
judaro26 <131112287+judaro26@users.noreply.github.com>
2023-06-18 17:49:14 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,27 @@ from fastapi import FastAPI, HTTPException
 from fastapi.responses import StreamingResponse
 import pandas as pd
 import gspread
+from fastapi.middleware.cors import CORSMiddleware
 
 
 
 app=FastAPI()
 
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 ### ENDPOINT TO CONFIRM THAT USER EXISTS ####
 @app.post("/authentication")
