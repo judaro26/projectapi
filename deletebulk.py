e749593b4ee5b5663e4d8086dacb17c327503930
judaro26 <131112287+judaro26@users.noreply.github.com>
2023-06-22 07:31:47 -0700

    Update deletebulk.py

--- deletebulk.py
+++ deletebulk.py
@@ -1,8 +1,9 @@
-from fastapi import FastAPI, HTTPException, Body
+from fastapi import FastAPI, HTTPException, Body,Request
 from fastapi.responses import StreamingResponse
 import pandas as pd
 import gspread
 from pydantic import BaseModel
+import re
 
 
 
@@ -42,21 +43,77 @@ def deleteuser(payload: dict = Body(...)):
         else:
             raise HTTPException(status_code=400,detail="THE USERNAME DOES NOT EXIST")
         
-@app.head("account-management")
+@app.head("/accountmanagement")
 def deleteuser():
-    raise HTTPException(status_code=403,detail='HEAD IS NOT ALLOWED FOR "deleteuser" ENDPOINT')
-@app.options("account-management")
+    raise HTTPException(status_code=403,detail='HEAD IS NOT ALLOWED FOR "accountmanagement" ENDPOINT')
+@app.options("/accountmanagement")
 def deleteuser():
-    raise HTTPException(status_code=403,detail='OPTIONS IS NOT ALLOWED FOR "deleteuser" ENDPOINT')
-@app.post("account-management")
+    raise HTTPException(status_code=403,detail='OPTIONS IS NOT ALLOWED FOR "accountmanagement" ENDPOINT')
+@app.post("/accountmanagement")
 def deleteuser():
-    raise HTTPException(status_code=403,detail='POST IS NOT ALLOWED FOR "deleteuser" ENDPOINT')
-@app.put("account-management")
+    raise HTTPException(status_code=403,detail='POST IS NOT ALLOWED FOR "accountmanagement" ENDPOINT')
+@app.put("/accountmanagement")
 def deleteuser():
-    raise HTTPException(status_code=403,detail='PUT IS NOT ALLOWED FOR "deleteuser" ENDPOINT')
-@app.patch("account-management")
-def deleteuser():
-    raise HTTPException(status_code=403,detail='PATCH IS NOT ALLOWED FOR "deleteuser" ENDPOINT')
-@app.get("account-management")
+    raise HTTPException(status_code=403,detail='PUT IS NOT ALLOWED FOR "accountmanagement" ENDPOINT')
+@app.get("/accountmanagement")
 def deleteuser():
     raise HTTPException(status_code=200,detail='SUCCESS')
+
+
+
+
+### ENDPOINT TO DELETE USER ACCOUNT ###
+@app.patch("/accountmanagement")
+def patchuser(targerUsername:str| None=None,targetpassword:str| None = None,targetname:str| None = None,payload: dict = Body(...)):
+    SHEET_ID='1XyE3KPBlM4AIqFHEUYkmyLxKvun6RnUFg92BeQMz4M0'
+    TODELETESHEET_NAME="Username"
+    SHEET_NAME='Management User DB'
+    if ('user') not in payload:
+        raise HTTPException(status_code=400,detail='YOU HAVE NOT ENTERED A USER')
+    if ('password') not in payload:
+        raise HTTPException(status_code=400,detail='YOU HAVE NOT ENTERED A PASSWORD')    
+    if ('delete') not in payload:
+        raise HTTPException(status_code=400,detail="YOU ARE MISSING THE DELETE OBJECT")
+    else:
+        username= payload['user']
+        password= payload['password'] 
+        delete= payload['delete'] 
+        gc = gspread.service_account('credentials.json')
+        spreadsheet=gc.open_by_key(SHEET_ID)
+        worksheet=spreadsheet.worksheet(SHEET_NAME)
+        tobeupdated=spreadsheet.worksheet(TODELETESHEET_NAME)
+        usercell=worksheet.find(username)
+        usernamevalue= worksheet.cell(usercell.row,usercell.col).value
+        passwordvalue=worksheet.cell(usercell.row,usercell.col+1).value
+        yesvalue=worksheet.cell(usercell.row,usercell.col+2).value
+        if username in usernamevalue and password in passwordvalue and delete in yesvalue:
+                newuserlist=[targerUsername,targetpassword,targetname]
+                if newuserlist[0]!=None:
+                    currentuser=str(newuserlist[0])
+                    criteria_re = re.compile(currentuser)
+                    coordinates = tobeupdated.find(criteria_re)
+                    if 'newusername' in payload:
+                        tobeupdated.update_cell(coordinates.row,coordinates.col,value=payload['newuser'])
+                        raise HTTPException(status_code=200,detail="USER UPDATED TO "+ payload['newuser'])
+                    else:
+                        raise HTTPException(status_code=207,detail="YOU FORGOT TO INCLUDE A NEW USER IN THE BODY OF YOUR CALL")
+                elif newuserlist[0]==None:
+                    raise HTTPException(status_code=207,detail="YOU DIDN'T ENTER A VALID 'targetUsername:True' PARAMETER")
+                if newuserlist[1]!=None:
+                    currentuser=str(newuserlist[0])
+                    criteria_re = re.compile(currentuser)
+                    coordinates = tobeupdated.find(criteria_re)
+                    if 'newpassword' in payload:
+                        tobeupdated.update_cell(coordinates.row,coordinates.col,value=payload['newuser'])
+                        raise HTTPException(status_code=200,detail="USER UPDATED TO "+ payload['newuser'])
+                    else:
+                        raise HTTPException(status_code=207,detail="YOU FORGOT TO INCLUDE A NEW PASSWORD IN THE BODY OF YOUR CALL")
+                else:
+                    raise HTTPException(status_code=207,detail="YOU DIDN'T ENTER A VALID 'targetpassword:True' PARAMETER")               
+                
+                
+        elif username in usernamevalue and password not in passwordvalue:
+            raise HTTPException(status_code=401,detail="YOU HAVE ENTERED THE WRONG PASSWORD")
+        else:
+            raise HTTPException(status_code=400,detail="THE USERNAME DOES NOT EXIST")
+        
